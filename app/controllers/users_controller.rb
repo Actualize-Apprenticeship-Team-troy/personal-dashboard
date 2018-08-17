ad5e620182b73d7790105bb7a7709b65f766082f
ZhenyaAK <ekolessova@gmail.com>
2018-08-16 22:05:42 -0400

    Card 1 SignUp page Styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @testing = "This is a test"
   end
 
   def create
-    user = User.create(username: Faker::Name, email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+   user = User.new(user_params)
     if user.save
       flash[:success] = "Successfully signed up"
       redirect_to '/'
     else
       flash[:warning] = 'Form is invalid'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,
+             :email,
+             :password)
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
+  # has_secure_password
+  attr_accessor :password
   validates :username, presence: true, length: { minimum: 3 }, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/,
    message: "is not an email" }
-  validates :password, :presence => true, :confirmation => true, :length => {minimum: 6}, :on => :create
+  validates :password, :presence => true, :confirmation => true, length: { in: 6..10 }, :on => :create
 end
